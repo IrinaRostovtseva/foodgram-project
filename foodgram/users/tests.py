f4a45a7c881fee6dca1498506bcaf3ad30836ae6
IrinaRostovtseva <ni8003@yandex.ru>
2020-09-10 00:33:56 +0300

    Mends formating.

--- foodgram/users/tests.py
+++ foodgram/users/tests.py
@@ -1,7 +1,2 @@
-from django.test import TestCase, Client
-from django.urls import reverse
-
-
-class TestSubscription(TestCase):
-    def setUp(self):
-        self.client = Client()
+# from django.test import TestCase, Client
+# from django.urls import reverse
