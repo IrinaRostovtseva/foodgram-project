adc9694a2512f95423b3957d1843909ca154a12b
IrinaRostovtseva <ni8003@yandex.ru>
2020-09-11 18:50:33 +0300

    Removes dict generation and mends file writing.

--- foodgram/recipes/create_file.py
+++ foodgram/recipes/create_file.py
@@ -1,16 +1,10 @@
 def write_into_file(filename, ingredients):
-    products = {}
-    for item in ingredients:
-        title = item['ingredients__title']
-        amount = item['ingredient__amount']
-        if title not in products:
-            unit = item['ingredients__unit']
-            products[title] = [amount, unit]
-        else:
-            products[title][0] += amount
     with open(filename, 'w') as shop_list:
-        shop_list.writelines(' Продукт (ед.измерения) - количество' + '\n')
-        shop_list.writelines('------------------------------------' + '\n')
-        for key in products:
-            line = f'{key} ({products[key][1]}) - {products[key][0]}'
-            shop_list.writelines('+ ' + line + '\n')
+        shop_list.write(' Продукт (ед.измерения) - количество \n')
+        shop_list.write('-------------------------------------\n')
+        for ingredient in ingredients:
+            title = ingredient['ingredient__title']
+            unit = ingredient['ingredient__unit']
+            amount = ingredient['amount__sum']
+            line = f'{title} ({unit}) - {amount}'
+            shop_list.write('+ ' + line + '\n')
